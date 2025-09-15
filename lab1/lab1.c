8e66892d5cd663b829e75e6272bf52fbeb5a20e6
Joshua-Justus <jja140@sfu.ca>
2025-09-15 00:04:35 -0700

    Lab 1 work

---
+++ lab1/lab1.c
@@ -0,0 +1,26 @@
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main() {
+  char *buff = NULL;
+  size_t size = 0;
+  ssize_t num_char;
+  printf("Please enter some text: ");
+  num_char = getline(&buff, &size, stdin);
+  if (num_char == -1) {
+    printf("Error reading input.");
+    free(buff);
+    exit(1);
+  }
+  printf("Tokens: \n");
+  char *saveptr;
+  char *token;
+  token = strtok_r(buff, " ", &saveptr);
+  while (token != NULL) {
+    printf("%s\n", token);
+    token = strtok_r(NULL, " ", &saveptr);
+  }
+  free(buff);
+  return 0;
+}
